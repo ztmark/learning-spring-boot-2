7cf6d1a9ba44ccad2f4c95541c46c001a8edde4a
Mark <shuyumark@gmail.com>
2017-12-04 15:19:36 +0800

    init

---
+++ src/main/java/io/github/ztmark/learningspringboot2/LearningSpringBoot2Application.java
@@ -0,0 +1,12 @@
+package io.github.ztmark.learningspringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBoot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSpringBoot2Application.class, args);
+    }
+}
